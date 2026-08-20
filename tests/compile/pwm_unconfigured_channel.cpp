3d81f7a9d80f4b1f3d5bd04d6c59298a8ab6dc11
Alexander Lymanets <lymanets@codevog.com>
2026-08-20 15:38:40 +0300

    Add pwm_unconfigured_channel test

---
+++ tests/compile/pwm_unconfigured_channel.cpp
@@ -0,0 +1,16 @@
+#include <cpphal.hpp>
+
+using namespace hal::literals;
+
+using PwmTimer = hal::timer::Pwm<
+  3,
+  hal::timer::config::Pwm<
+    hal::timer::options::Frequency<20_kHz>,
+    hal::timer::options::InitialDuty<2>,
+    hal::timer::options::Channel<1>
+  >
+>;
+
+int main() {
+  PwmTimer::set_duty<2>(50);
+}
