991c37dc3dd3a8243a6db07f5d02bc2852a15150
Alexander Lymanets <lymanets@codevog.com>
2026-08-20 15:38:22 +0300

    Update pwm_duplicate_channel

--- tests/compile/pwm_duplicate_channel.cpp
+++ tests/compile/pwm_duplicate_channel.cpp
@@ -2,36 +2,17 @@
 
 using namespace hal::literals;
 
-using SystemTimer = hal::timer::System<hal::timer::Unit::msec>;
-
-using System = hal::SystemConfigurator<
-  hal::rcc::PLL<8_MHz>,
-  hal::rcc::Sysclk<72_MHz>,
-  hal::rcc::AHB<72_MHz>,
-  hal::rcc::APB1<36_MHz>,
-  hal::rcc::APB2<72_MHz>
->;
-
-using GpioConfig = hal::gpio::Configurator<>;
-
 using PwmTimer = hal::timer::Pwm<
   3,
   hal::timer::config::Pwm<
     hal::timer::options::Frequency<20_kHz>,
     hal::timer::options::Channel<2>,
     hal::timer::options::Channel<2>,
-    hal::timer::options::InitialDuty<20>,
-    hal::timer::options::pwm_mode::ActiveHigh
+    hal::timer::options::InitialDuty<20>
   >
 >;
 
-using BoardConfig = hal::Configurator<
-  System,
-  SystemTimer,
-  GpioConfig,
-  PwmTimer
->;
-
 int main() {
+  PwmTimer::set_duty<2>(50);
   return 0;
 }
